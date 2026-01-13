cf12a777da922eb42034acea3dddd9e9da529ff4
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-01-13 17:14:20 -0500

    springweb

--- src/main/java/com/example/Springboot1Application.java
+++ src/main/java/com/example/Springboot1Application.java
@@ -2,12 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Springboot1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Springboot1Application.class, args);
     }
 
+    @GetMapping
+    public String helloWorld(){
+
+        return "Hello World";
+    }
 }
